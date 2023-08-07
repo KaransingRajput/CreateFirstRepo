b06a78a81c17e7742e8248a206737ee307a96130
karan rajput <121344093+KaransingRajput@users.noreply.github.com>
2023-08-07 14:41:44 +0530

    initial commit

---
+++ src/com/example/revrese/ReverseStringDemo.java
@@ -0,0 +1,15 @@
+package com.example.revrese;
+
+public class ReverseStringDemo {
+public static void main(String[] args) {
+	
+final int i;
+i=20;
+int j=i+20;
+j=i+j;
+System.out.println(j);
+
+
+}
+
+}
